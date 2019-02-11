ad15c1e621f0905053f03b4a3c4d5b967ac7b36c
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-10 21:16:15 -0700

    Adds tests

--- spec/features/user_sees_siacoin_info_spec.rb
+++ spec/features/user_sees_siacoin_info_spec.rb
@@ -3,21 +3,17 @@ require 'rails_helper'
 describe "A visitor to our site" do
   it 'can see siacoin information' do
     visit '/'
-    expect(page).to have_content("miners")
+
+    expect(page).to have_content("Siacoin")
+    expect(page).to have_css("#price")
+    expect(page).to have_css("#last-week")
+    expect(page).to have_css("#icon")
+    expect(page).to have_css("#miners")
+    expect(page).to have_content("7d change")
+
+    expect(page).to have_css(".form")
+    fill_in 'coin_input', with: 500
+    click_on "Calculate"
+    expect(page).to have_content "value of your coins"
   end
 end
-
-
-# As a visitor to our app
-# when I visit '/'
-# I should see a heading for "Siacoin"
-# And I should see the current siacoin price in the following format:
-# if under 1 cent, ".02 cents", and if over 1 cent, "$0.01"
-#
-# I should also see a form
-# Where I can enter the amount of siacoin I have
-# and under that form I can see the value of my holdings of this coin in USD
-#
-# I should also see the performance of the coin's value over the last 7 days vs USD,
-# for example "7 day performance: +2.59%"
-# which should be green text if positive, and red text if negative.

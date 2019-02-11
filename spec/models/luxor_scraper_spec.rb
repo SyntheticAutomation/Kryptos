5ae77cc166e9d4624abdbee3aa60bae7abd7e741
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-11 12:25:06 -0700

    Adds test suite for Luxor Scraper

---
+++ spec/models/luxor_scraper_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'nokogiri'
+require 'open-uri'
+require 'watir'
+require 'pry'
+
+describe LuxorScraper, type: :model do
+  before(:each) do
+    wallet = ENV['sc_wallet']
+    @lux = LuxorScraper.new(wallet)
+    @daily_earnings = @lux.daily_earnings
+  end
+
+  it 'exists' do
+    expect(@lux).to be_an_instance_of LuxorScraper
+  end
+
+  describe 'instance methods' do
+
+    it '.daily_earnings' do
+      expect(@daily_earnings.is_a?(Float)).to eq(true)
+    end
+  end
+end
